605de95b15ca953bbb6f2c7fa09eb3b3c7161eb6
omid sha <code.omidsha@gmail.com>
2021-11-04 13:14:59 +0330

    fix unexpected crashes and add error handling

--- main.py
+++ main.py
@@ -15,28 +15,35 @@ tz = pytz.timezone('Asia/Tehran')
 
 i = 0
 alert_sens = 200
-chat_id = 1324884291
+sudo_id = 1324884291
+channel_id = -1001728669440
 while True:
-  r = req.get(url)
-  price = float(r.json()['price'])
+  try:
+    r = req.get(url)
+    price = float(r.json()['price'])
 
-  if i != 0:
+    if i != 0:
 
-    ekhtelaf = prev_pos - price
-    abs_ekhtelaf = abs(ekhtelaf)
+      ekhtelaf = prev_pos - price
+      abs_ekhtelaf = abs(ekhtelaf)
 
 
-    if abs_ekhtelaf > alert_sens:
-      now = datetime.datetime.now(tz)
-      payload = now.strftime('%b %d \n%H:%M (Tehran)')
-      if ekhtelaf < 0:
-        payload = f'{payload} \n🟢 Pumped Over 200$ \n💎 ${price}\n- @riikht'
-      else:
-        payload = f'{payload} \n🔴 Crashed Over 200$ \n💎 ${price}\n- @riikht'
-      bot.send_message(text=payload, chat_id=chat_id)
+      if abs_ekhtelaf > alert_sens:
+        now = datetime.datetime.now(tz)
+        payload = now.strftime('%b %d \n%H:%M (Tehran)')
+        if ekhtelaf < 0:
+          payload = f'{payload} \n🟢 Pumped Over 200$ \n💎 ${price}\n- @riikht'
+        else:
+          payload = f'{payload} \n🔴 Crashed Over 200$ \n💎 ${price}\n- @riikht'
+        bot.send_message(text=payload, chat_id=channel_id)
+        prev_pos = price
+    else:
+      i = 1
       prev_pos = price
-  else:
-    i = 1
-    prev_pos = price
 
-  time.sleep(5)
+  except req.ConnectionError as e:
+    bot.send_message(text=str(e), chat_id=sudo_id)
+
+  finally:
+    time.sleep(5)
+    continue
