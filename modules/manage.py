e6f90aefc2831122cc52ee3d253e914ef0ac8e89
omid sha <code.omidsha@gmail.com>
2021-11-06 19:39:34 +0330

    add utils

---
+++ modules/manage.py
@@ -0,0 +1,21 @@
+from telegram.ext import CommandHandler
+from telegram.ext import Updater
+import psutil
+
+
+updater = Updater(
+    token='2059581474:AAGvXelK7C2pwaOgO5dmFLfAKSruJxR8yqE', use_context=True)
+
+dispatcher = updater.dispatcher
+
+
+def status(update, context):
+    payload = f'Bot is running\nCPU usage: {psutil.cpu_percent(4)}%\nRam Usage: {psutil.virtual_memory()[2]}%\nUptime: '
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=payload)
+
+
+start_handler = CommandHandler('status', status)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
